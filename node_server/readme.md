0aea8b4ba00c59d86a1fe9b0c2d2722e3d8b850c
Patrick Martin <pux1987@gmail.com>
2016-02-01 21:59:19 -0700

    I implemented google sign-in. Yay Security!

---
+++ node_server/readme.md
@@ -0,0 +1,8 @@
+# Node JS Garage Control
+Usage: must create a file "keys.json" in the node_server (this) directory.
+Should be in the form of
+`{
+   "google_key": "<Google Api Key>"
+ }`
+
+ You can generate this by making an "OAuth Client Id" and selecting everything before the .apps.googleusercontent.com
--- node_server/routes/garage_route.js
+++ node_server/routes/garage_route.js
@@ -6,13 +6,15 @@ var express = require('express');
 var router = express.Router();
 
 var garage_control = require("../garage_control");
+var keys = require('../keys.json');
 
 function render_garage(req, res, next) {
     res.render(
         'garage_route',
         {
             title: 'Garage Control Center',
-            garage: garage_control
+            garage: garage_control,
+            client_id: keys.google_key
         });
 }
 
